cd8f5d1b9fdd9f7b89a7408815d8c565a71a4070
Gustavo Kath <gustavo.kath@sap.com>
2020-11-25 11:17:43 -0300

    fix: CORS according to working

--- api_gateway/secret_santa_api.tf
+++ api_gateway/secret_santa_api.tf
@@ -21,8 +21,9 @@ resource "aws_api_gateway_integration" "secret_santa_api_integration" {
    http_method = aws_api_gateway_method.secret_santa_api_method.http_method
 
    integration_http_method = "POST"
-   type                    = "AWS_PROXY"
+   type                    = "AWS"
    uri                     = var.secret_santa_lambda_invoke_arn
+   passthrough_behavior = "WHEN_NO_MATCH"
 }
 
 resource "aws_api_gateway_deployment" "secret_santa_api_deployment" {
@@ -67,21 +68,6 @@ resource "aws_api_gateway_method_response" "options_200" {
     }
     depends_on = [aws_api_gateway_method.options_method]
 }
-resource "aws_api_gateway_method_response" "options_404" {
-   rest_api_id   = aws_api_gateway_rest_api.secret_santa_rest_api.id
-   resource_id   = aws_api_gateway_resource.secret_santa_api_resource.id
-   http_method   = aws_api_gateway_method.options_method.http_method
-   status_code   = "404"
-   response_models = {
-      "application/json" = "Empty"
-   }
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Headers" = true,
-        "method.response.header.Access-Control-Allow-Methods" = true,
-        "method.response.header.Access-Control-Allow-Origin" = true
-    }
-    depends_on = [aws_api_gateway_method.options_method]
-}
 
 resource "aws_api_gateway_integration" "options_integration" {
    rest_api_id   = aws_api_gateway_rest_api.secret_santa_rest_api.id
@@ -104,24 +90,12 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
    http_method   = aws_api_gateway_method.options_method.http_method
    status_code   = aws_api_gateway_method_response.options_200.status_code
    response_parameters = {
-      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers'",
+      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-To'",
       "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
       "method.response.header.Access-Control-Allow-Origin" = "'*'"
+   }
+   response_templates = {
+     "application/json" = ""
    }
     depends_on = [aws_api_gateway_method_response.options_200]
 }
-
-resource "aws_api_gateway_integration_response" "options_integration_404_response" {
-   rest_api_id   = aws_api_gateway_rest_api.secret_santa_rest_api.id
-   resource_id   = aws_api_gateway_resource.secret_santa_api_resource.id
-   http_method   = aws_api_gateway_method.options_method.http_method
-   status_code   = aws_api_gateway_method_response.options_404.status_code
-   selection_pattern = ".*\"status\":404.*"
-   response_parameters = {
-      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers'",
-      "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
-      "method.response.header.Access-Control-Allow-Origin" = "'*'"
-   }
-    depends_on = [aws_api_gateway_method_response.options_404]
-}
-
