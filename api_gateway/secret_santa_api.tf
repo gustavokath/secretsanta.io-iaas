526682d91102cf4a6e8626aa92d4063b0e5f71f1
Gustavo Kath <gustavo.kath@sap.com>
2020-11-25 02:47:21 -0300

    Add options in api gateway for CORS

--- api_gateway/secret_santa_api.tf
+++ api_gateway/secret_santa_api.tf
@@ -44,3 +44,48 @@ resource "aws_lambda_permission" "secret_santa_api_lambda_permission" {
    # within the API Gateway REST API.
    source_arn = "${aws_api_gateway_rest_api.secret_santa_rest_api.execution_arn}/*/*"
 }
+
+resource "aws_api_gateway_method" "options_method" {
+   rest_api_id   = aws_api_gateway_rest_api.secret_santa_rest_api.id
+   resource_id   = aws_api_gateway_resource.secret_santa_api_resource.id
+   http_method   = "OPTIONS"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options_200" {
+   rest_api_id   = aws_api_gateway_rest_api.secret_santa_rest_api.id
+   resource_id   = aws_api_gateway_resource.secret_santa_api_resource.id
+   http_method   = aws_api_gateway_method.options_method.http_method
+   status_code   = "200"
+   response_models = {
+      "application/json" = "Empty"
+   }
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Origin" = true
+    }
+    depends_on = [aws_api_gateway_method.options_method]
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+   rest_api_id   = aws_api_gateway_rest_api.secret_santa_rest_api.id
+   resource_id   = aws_api_gateway_resource.secret_santa_api_resource.id
+   http_method   = aws_api_gateway_method.options_method.http_method
+   type          = "MOCK"
+   depends_on = [aws_api_gateway_method.options_method]
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+   rest_api_id   = aws_api_gateway_rest_api.secret_santa_rest_api.id
+   resource_id   = aws_api_gateway_resource.secret_santa_api_resource.id
+   http_method   = aws_api_gateway_method.options_method.http_method
+   status_code   = aws_api_gateway_method_response.options_200.status_code
+   response_parameters = {
+      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'",
+      "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
+      "method.response.header.Access-Control-Allow-Origin" = "'*'"
+   }
+    depends_on = [aws_api_gateway_method_response.options_200]
+}
+
